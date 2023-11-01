d8318a1b4454e132e308952906860dd8062735b5
Alexsander <afgoueia90@gmail.com>
2023-11-01 09:54:03 +0000

    Project created

---
+++ app/src/main/java/com/example/gastoviagem/MainActivity.kt
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2023. Created by Alexsander at 11/1. All rights reserved.
+ * GitHub: https://github.com/alexsanderfer/
+ * Portfolio: https://alexsanderfer.netlify.app/
+ */
+
+package com.example.gastoviagem
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
