6f5d23e4d3d32b87954f99e8b4117a64cd225083
Alexsander <afgoueia90@gmail.com>
2023-11-02 20:36:39 +0000

    Project ended

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2023. Created by Alexsander at 11/2. All rights reserved.
+ * GitHub: https://github.com/alexsanderfer/
+ * Portfolio: https://alexsanderfer.netlify.app/
+ */
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
@@ -34,6 +40,7 @@ android {
         jvmTarget = "1.8"
     }
     buildFeatures {
+        viewBinding = true
         compose = true
     }
     composeOptions {
@@ -48,7 +55,7 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.8.0")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
@@ -57,6 +64,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     testImplementation("junit:junit:4.13.2")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
--- app/src/main/java/com/example/gastoviagem/MainActivity.kt
+++ app/src/main/java/com/example/gastoviagem/MainActivity.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023. Created by Alexsander at 11/1. All rights reserved.
+ * Copyright (c) 2023. Created by Alexsander at 11/2. All rights reserved.
  * GitHub: https://github.com/alexsanderfer/
  * Portfolio: https://alexsanderfer.netlify.app/
  */
@@ -7,11 +7,61 @@
 package com.example.gastoviagem
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.activity.ComponentActivity
+import com.example.gastoviagem.databinding.ActivityMainBinding
 
-class MainActivity : ComponentActivity() {
+class MainActivity : ComponentActivity(), View.OnClickListener {
+    private lateinit var binding: ActivityMainBinding
+
+    /*
+     Função responsável por fazer a criação da Activity.
+    * */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Adiciona evento ao elemento da interface.
+        binding.buttonCalculate.setOnClickListener(this)
+    }
+
+    /*
+    * Função responsável por tratar qualquer evento de click nos elementos.
+    * */
+    override fun onClick(view: View) {
+        if (view.id == R.id.button_calculate) {
+            calculate()
+        }
+    }
+
+    /*
+    * Função responsável por validar se os elementos receberam e retornam valores válidos.
+    * */
+    fun isValid(): Boolean {
+        return with(binding) {
+            editDistance.text.toString().isNotEmpty() &&
+                    editPrice.text.toString().isNotEmpty() &&
+                    editAutonomy.text.toString().isNotEmpty() &&
+                    editAutonomy.text.toString().toFloatOrNull() != null &&
+                    editAutonomy.text.toString().toFloat() != 0f
+        }
+    }
+
+    /*
+    * Função responsável por calcular o Gasto das viagens, se baseando em: distância, preço por litro e autonomia do veículo.
+    * */
+    private fun calculate() {
+        if (isValid()) {
+            val distance = binding.editDistance.text.toString().toFloat()
+            val price = binding.editPrice.text.toString().toFloat()
+            val autonomy = binding.editAutonomy.text.toString().toFloat()
+
+            val totalValue = (distance * price) / autonomy
+            binding.textTotalValue.text = getString(R.string.r, "%.2f".format(totalValue))
+        } else {
+            Toast.makeText(this, getString(R.string.validation_fill_all_fields), Toast.LENGTH_SHORT).show()
+        }
     }
 }
